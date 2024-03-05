1d1867246170dc22c210ff197308e1738abeeed9
Hiroki Ikeuchi <hikeuchi@morisawa.co.jp>
2024-03-05 15:06:24 +0900

    fix: error for isort

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import math
+import os
+
+
 def sample_print() -> None:
+    print(math.pi)
+    print(os.name)
     print("Hello, Python!")
 
 
 def main() -> None:
+    sample_print()
     print("goodbye, Python!")
     print("Hello, Python!")
 
