4e6f3b5f47b47ca7999b99dd72f18d2742ed86c3
Hiroki Ikeuchi <hikeuchi@morisawa.co.jp>
2024-03-05 11:04:29 +0900

    add resources

---
+++ main.py
@@ -0,0 +1,10 @@
+def sample_print():
+    print("Hello, Python!")
+
+def main():
+    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    print("Hello, Python!")
+
+
+if __name__ == "__main__":
+    main()
